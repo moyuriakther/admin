e5986655e8e01b38eb307f1e3b6667c0c164b3be
Moyuri Akther <akthermoyuri@gmail.com>
2023-08-30 00:43:02 +0600

    up before change pc

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3raabDEz2NKCGehqqignILKc1TzonNsU",
+  authDomain: "vite-shop-admin.firebaseapp.com",
+  projectId: "vite-shop-admin",
+  storageBucket: "vite-shop-admin.appspot.com",
+  messagingSenderId: "81567684839",
+  appId: "1:81567684839:web:f86711feb677f36258aa42",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
