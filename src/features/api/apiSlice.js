c9a04c7927a368d1ff7b26e58a8a1c13d1fd9fab
Moyuri Akther <75539860+moyuriakther@users.noreply.github.com>
2024-07-05 00:08:00 +0600

    Update apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shop-backend-bpgi.onrender.com",
+    baseUrl: "https://shop-backend-flax.vercel.app",
     prepareHeaders: async (headers, { getState }) => {
       const tokenInfo = getState().auth;
       console.log(tokenInfo);
